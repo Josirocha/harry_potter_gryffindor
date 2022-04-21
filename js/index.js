25dd8cd4d5f5b1308b647aa14fe579cd27b19952
Josirocha <josi1992@gmail.com>
2022-04-21 13:20:25 -0300

    ajustes visuais e ajuste na função de escolher personagem

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 function escolherPersonagens() {
     location.assign("./pages/personagens.html")
 }
-
-function harry() {
-    location.assign("../pages/harry_potter/fase1.html")
-}
-function hermione() {
-    location.assign("../pages/hermione/fase1.html")
-}
-function ron() {
-   location.assign("../pages/ron/fase1.html") 
+function selecionarPersonagem(personagem) {
+    location.assign(`../pages/${personagem}/fase1.html`)
 }
